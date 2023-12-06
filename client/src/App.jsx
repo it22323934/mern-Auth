731af54994b751c92133936a17ccd3ee50914c50
Asiri Jayawardena <it22323934@my.sliit.lk>
2023-12-06 11:58:56 +0530

    Asiri

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,11 @@ import About from './pages/About'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import UserProfile from './pages/UserProfile'
+import Header from './components/Header'
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
