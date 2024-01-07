db3436dd97aa267b1ae6c1de561943fadcd6f241
Asiri Jayawardena <it22323934@my.sliit.lk>
2024-01-07 12:54:01 +0530

    Asiri

--- API/routes/user.route.js
+++ API/routes/user.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import {test} from '../controller/user.controller.js';
+
 const router=express.Router();
 
 router.get('/',test);
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './api/routes/user.route.js';
+import authRoutes from './api/routes/auth.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL)
     .then(()=>console.log('DB connected'))
@@ -14,3 +15,4 @@ app.listen(3000,()=>{
 });
 
 app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
