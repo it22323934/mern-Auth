5c3b3880637403cda8a6d1ff4df736ab3a8f8722
Asiri Jayawardena <it22323934@my.sliit.lk>
2024-01-08 02:27:50 +0530

    bug fix-Asiri

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ mongoose.connect(process.env.MONGO_URL)
     .then(()=>console.log('DB connected'))
     .catch(err=>console.log(err));
 
-const _dirname=path.resolve();
+const __dirname=path.resolve();
 const app=express();
 
-app.use(express.static(path.join(_dirname,'/client/dist')))
+app.use(express.static(path.join(__dirname,'/client/dist')))
 
 app.get('*',(req,res)=>{
-res.sendFile(path.join(_dirname,'client','dist','index.html'));
+res.sendFile(path.join(__dirname,'client','dist','index.html'));
 });
 
 
