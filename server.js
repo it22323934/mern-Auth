0ce64ac016b96580a40075100f43ce505a71503c
Asiri Jayawardena <it22323934@my.sliit.lk>
2024-01-07 13:19:55 +0530

    Create SignUp APIR Route-Asiri

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ mongoose.connect(process.env.MONGO_URL)
     .catch(err=>console.log(err));
     
 const app=express();
-
+app.use(express.json());
 app.listen(3000,()=>{
     console.log('Server is running on port 3000!');
 });
